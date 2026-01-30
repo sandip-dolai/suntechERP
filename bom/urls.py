eaa77beb95ce3887d3fbd618a33ff57563e1abe8
Sandip Dolai <sandip1998dolai@gmail.com>
2026-01-30 16:56:13 +0530

    add bom urls

--- bom/urls.py
+++ bom/urls.py
@@ -1,5 +1,23 @@
 from django.urls import path
+from . import views
 
 app_name = "bom"
 
-urlpatterns = []
+urlpatterns = [
+    # -----------------------------
+    # BOM LIST
+    # -----------------------------
+    path("", views.bom_list, name="bom_list"),
+    # -----------------------------
+    # BOM CREATE
+    # -----------------------------
+    path("create/", views.bom_create, name="bom_create"),
+    # -----------------------------
+    # BOM DETAIL / VIEW
+    # -----------------------------
+    path("<int:pk>/", views.bom_detail, name="bom_detail"),
+    # -----------------------------
+    # AJAX: LOAD PO ITEMS
+    # -----------------------------
+    path("ajax/po-items/", views.ajax_load_po_items, name="ajax_load_po_items"),
+]
