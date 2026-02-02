0e71ab20df005a2b011684b8893aee59d02e4020
Sandip Dolai <sandip1998dolai@gmail.com>
2026-02-02 13:19:42 +0530

    notification views add

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from notifications.models import Notification
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+users = User.objects.filter(is_active=True)
+
+Notification.objects.bulk_create(
+    [
+        Notification(
+            user=user,
+            title="New Purchase Order Created",
+            message=f"PO {po.po_number} has been created.",
+            url=f"/po/{po.id}/processes/",
+        )
+        for user in users
+    ]
+)
