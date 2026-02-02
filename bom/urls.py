6f0e45787e7d7e3c8773ec9a256f58b403dfe558
Sandip Dolai <sandip1998dolai@gmail.com>
2026-02-02 17:24:37 +0530

    feat(bom): add bom report and excel urls

--- bom/urls.py
+++ bom/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = "bom"
 
 urlpatterns = [
-    path("", views.bom_list, name="bom_list"),                 # /bom/
-    path("create/", views.bom_create, name="bom_create"),      # /bom/create/
-    path("<int:pk>/edit/", views.bom_edit, name="bom_edit"),   # /bom/1/edit/
-    path("<int:pk>/", views.bom_detail, name="bom_detail"),    # /bom/1/
+    path("", views.bom_list, name="bom_list"),  # /bom/
+    path("create/", views.bom_create, name="bom_create"),  # /bom/create/
+    path("<int:pk>/edit/", views.bom_edit, name="bom_edit"),  # /bom/1/edit/
+    path("<int:pk>/", views.bom_detail, name="bom_detail"),  # /bom/1/
     path("ajax/po-items/", views.ajax_load_po_items, name="ajax_load_po_items"),
     path("<int:pk>/delete/", views.bom_delete, name="bom_delete"),
+    # BOM REPORT
+    path("report/", views.bom_report, name="bom_report"),
+    path("report/excel/", views.bom_report_excel, name="bom_report_excel"),
 ]
